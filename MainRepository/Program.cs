b30f8acac7e2facc6a34d465bd9305029d2f0e42
karthickraj <karthickraj.s@metamation.com>
2023-08-08 19:09:41 +0530

    Implemented PriorityQueue

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,62 @@
-﻿internal class Program
+﻿// Implementation of PriroityQueue using Binary Heap
+var pq = new PriorityQueue<int>();
+
+pq.Enqueue(25);
+pq.Enqueue(8);
+Console.WriteLine(pq.Dequeue());
+pq.Enqueue(27);
+pq.Enqueue(3);
+Console.WriteLine(pq.Dequeue());
+pq.Enqueue(1);
+pq.Enqueue(2);
+pq.Enqueue(7);
+foreach (var item in pq.data)
+    Console.Write(item + " ");
+Console.WriteLine();
+
+public class PriorityQueue<T> where T : IComparable<T>
 {
-    private static void Main(string[] args)
+    public List<T> data = new List<T>();
+    // Element inserting function maintain min heap
+    public void Enqueue(T item)
     {
-        Console.WriteLine("Hello, World!");
+        data.Add(item);
+        int ci = data.Count - 1;
+        while (ci > 0)
+        {
+            int pi = (ci - 1) / 2;
+            if (data[ci].CompareTo(data[pi]) >= 0) break;
+            T tmp = data[ci];
+            data[ci] = data[pi];
+            data[pi] = tmp;
+            ci = pi;
+        }
     }
+    // Element deleting function maintain min heap
+    public T Dequeue()
+    {
+        int li = data.Count - 1;
+        T top = data[0];
+        data[0] = data[li];
+        data.RemoveAt(li);
+        --li;
+        int pi = 0;
+        while (true)
+        {
+            int ci = pi * 2 + 1;
+            if (ci > li) break;
+            int rc = ci + 1;
+            if (rc <= li && data[rc].CompareTo(data[ci]) < 0)
+                ci = rc;
+            if (data[pi].CompareTo(data[ci]) <= 0) break;
+            T tmp = data[pi];
+            data[pi] = data[ci];
+            data[ci] = tmp;
+            pi = ci;
+
+        }
+        return top;
+    }
+    public bool IsEmpty => mCount == 0;
+    int mCount;
 }
