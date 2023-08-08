1ec77de787740d425da415e939eabb3f0661c3dc
karthickraj <karthickraj.s@metamation.com>
2023-08-08 17:48:21 +0530

    Implemented Spell Bee

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,59 @@
-﻿internal class Program
+﻿// Spell Bee Program
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var words = File.ReadAllLines("c:/etc/words.txt");
+char[] seed = { 'U', 'X', 'L', 'T', 'A', 'E', 'N' };
+List<string> valid = new();
+foreach (var word in words)
+    if (IsValid(word)) valid.Add(word);
+for (int i = 0; i < valid.Count - 1; i++)
 {
-    private static void Main(string[] args)
+    for (int j = i + 1; j < valid.Count; j++)
     {
-        Console.WriteLine("Hello, World!");
+        if (InOrder(valid[i], valid[j]))
+        {
+            (valid[j], valid[i]) = (valid[i], valid[j]);
+        }
+
     }
+}
+int total = 0;
+foreach (var w in valid)
+{
+    if (IsPangram(w)) Console.ForegroundColor = ConsoleColor.Green;
+    int score = GetScore(w);
+    Console.WriteLine($"{score,3}. {w}");
+    Console.ResetColor();
+    total += score;
+
+}
+Console.WriteLine($"---\n{total,3} total");
+// Checking the given string is valid or not
+bool IsValid(string word)
+{
+    if ((word.Length >= 4) && (word.Contains(seed[0])) && (word.All(seed.Contains))) return true;
+    return false;
+
+}
+// Checking the given string is pangram or not
+bool IsPangram(string word)
+{
+    return seed.All(word.Contains);
+}
+// Compute the score of given string
+int GetScore(string word)
+{
+    if (word.Length <= 4) return 1;
+    else if (IsPangram(word)) return word.Length + 7;
+    else return word.Length;
+}
+// Comparing the score of given string
+bool InOrder(string a, string b)
+{
+    int sa = GetScore(a), sb = GetScore(b);
+    if (sa != sb) return sa > sb;
+    return a.CompareTo(b) < 0;
 }
