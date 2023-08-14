8a98757cabf7beab594195b89d3e2e15858509c3
karthickraj <karthickraj.s@metamation.com>
2023-08-14 18:09:45 +0530

    Palindrome program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿// Program to find the given word is palindrome or not.
+Console.Write("Enter the word or sentence: ");
+string str = Console.ReadLine().ToLower().Replace(" ", "");
+string rev = "";
+for (int i = str.Length - 1; i >= 0; i--)
+    rev += str[i];
+if (rev == str)
+    Console.Write("Palindrome");
+else
+    Console.Write("Not a palindrome");
