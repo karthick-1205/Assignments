3dd3c8b797642878771e673e9dcea54ce5a96ea2
karthickraj <karthickraj.s@metamation.com>
2023-08-14 17:27:46 +0530

    Gcd and Lcm of two numbers

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿// Program to find the gcd and lcm of two numbers.
+Console.Write("Enter the first number: ");
+int n1, n2, x, y, temp, gcd, lcm;
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+x = n1;
+y = n2;
+while (y > 0)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    temp = y;
+    y = x % y;
+    x = temp;
 }
+gcd = x;
+lcm = (n1 * n2) / gcd;
+Console.WriteLine("The GCD of two number is " + gcd);
+Console.WriteLine("The LCM of two number is " + lcm);
