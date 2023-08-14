6f455a04f4e61935aa02681fb5353a53a84c134d
karthickraj <karthickraj.s@metamation.com>
2023-08-14 18:13:39 +0530

    Prime program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿//Program to find the prime number or not.
+Console.Write("Enter the Number: ");
+string number;
+number = Console.ReadLine();
+Prime(number);
+static void Prime(string number)
 {
-    private static void Main(string[] args)
+    int result;
+    if (int.TryParse(number, out result))
     {
-        Console.WriteLine("Hello, World!");
+        int divisors = 0;
+        for (int i = 1; i <= result; i++)
+        {
+            if (result % i == 0)
+                divisors++;
+        }
+        if (divisors == 2)
+            Console.Write($"{result} is the prime number");
+        else
+            Console.Write($"{result} is the not a prime number");
+    }
+    else
+    {
+        Console.WriteLine("Enter the valid input");
+        Console.Write("Enter the Number: ");
+        number = Console.ReadLine();
+        Prime(number);
     }
 }
