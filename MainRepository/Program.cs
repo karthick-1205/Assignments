a3270d0f062f585acdad2c75000a016201737a8e
karthickraj <karthickraj.s@metamation.com>
2023-08-14 17:15:37 +0530

    Decimal number conversion

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿// Program to convert the decimal number to binary and hexadecimal number.
+Console.Write("Enter the number: ");
+int number, num, num1;
+number = Convert.ToInt32(Console.ReadLine());
+num = num1 = number;
+List<int> binary = new();
+List<char> hexadecimal = new();
+while (number > 0)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    int temp = number % 2;
+    binary.Add(temp);
+    number /= 2;
 }
+while (num1 > 0)
+{
+    int temp = num1 % 16;
+    if (temp < 10)
+    {
+        temp = temp + 48;
+        hexadecimal.Add((char)temp);
+    }
+    else
+    {
+        temp = temp + 55;
+        hexadecimal.Add((char)temp);
+    }
+    num1 /= 16;
+}
+binary.Reverse();
+hexadecimal.Reverse();
+Console.Write($"The binary number of {num} is ");
+foreach (var result in binary)
+    Console.Write(result);
+Console.Write($"\nThe hexadecimal number of {num} is ");
+foreach (var result in hexadecimal)
+    Console.Write(result);
