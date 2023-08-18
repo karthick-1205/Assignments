9ef7bd53393484e26519a0d6dd74d01a117c8938
karthickraj <karthickraj.s@metamation.com>
2023-08-18 18:20:39 +0530

    Multiplication table

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,5 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿// Program to print the multiplication table of the given number
+Console.Write("Enter the Number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
+    Console.WriteLine($"{n} * {i} = {n * i}");
