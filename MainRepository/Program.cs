d4b4a49e55ccc72ac33215b97b7f95d29dc77f5d
karthickraj <karthickraj.s@metamation.com>
2023-08-18 18:35:09 +0530

    Palindrome number

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿// Program to find the given number is palindrome number or not
+Console.Write("Enter the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int reverse = 0, temp;
+temp = n;
+while (temp > 0)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    int rem = temp % 10;
+    reverse = reverse * 10 + rem;
+    temp /= 10;
 }
+if (n == reverse)
+    Console.WriteLine($"{reverse} is Palindrome number");
+else
+    Console.WriteLine($"{reverse} is not a Palindrome number");
