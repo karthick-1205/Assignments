016d61361203226f5ed205206772c30ec282717f
karthickraj <karthickraj.s@metamation.com>
2023-08-24 12:59:29 +0530

    Implemented star pattern program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿// Program to print a diamond using an asterisk
+Console.Write("Enter the no of rows: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
 {
-    private static void Main(string[] args)
+    for (int j = 0; j < n; j++)
     {
-        Console.WriteLine("Hello, World!");
+        if (i + j >= n - 1) Console.Write("* ");
+        else Console.Write("  ");
     }
+    for (int j = 0; j < n; j++)
+    {
+        if (i > j) Console.Write("* ");
+        else Console.Write("  ");
+    }
+    Console.WriteLine();
+}
+for (int i = 1; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i <= j) Console.Write("* ");
+        else Console.Write("  ");
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (i + j < n - 1) Console.Write("* ");
+        else Console.Write("  ");
+    }
+    Console.WriteLine();
 }
