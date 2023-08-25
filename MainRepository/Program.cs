0d69d7709c90fefcc5a95229f91999bbc4129035
karthickraj <karthickraj.s@metamation.com>
2023-08-25 12:47:41 +0530

    Implemented digital root number

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿// Program to find the digital root of given number.
+for (; ; )
 {
-    private static void Main(string[] args)
+    Console.Write("Enter the Number or press [e] to exit: ");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "e") break;
+    int.TryParse(input, out int n);
+    int sum = 0;
+    if (n < 0) Console.WriteLine("Enter the valid input");
+    else
     {
-        Console.WriteLine("Hello, World!");
+        int temp = n;
+        while (temp > 0)
+        {
+            int rem = temp % 10;
+            sum += rem;
+            temp /= 10;
+            if (temp == 0)
+            {
+                if (sum > 9)
+                {
+                    temp = sum;
+                    sum = 0;
+                }
+            }
+        }
+        Console.WriteLine($"The digital root number of {n} is {sum}");
     }
 }
