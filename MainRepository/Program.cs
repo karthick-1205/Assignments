be8581bd5701caf4e382ca488f8c779867887ef0
karthickraj <karthickraj.s@metamation.com>
2023-08-28 15:39:07 +0530

    Implemented String permutation

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿//String permutation
+Console.Write("Enter the string: ");
+string str = Console.ReadLine();
+HashSet<string> result = GetPermutations(str);
+Console.WriteLine($"Permutation of {str} are " + string.Join(" ", result));
+static HashSet<string> GetPermutations(string str)
 {
-    private static void Main(string[] args)
+    HashSet<string> permutations = new ();
+    if (str.Length == 0)
     {
-        Console.WriteLine("Hello, World!");
+        permutations.Add("");
+        return permutations;
     }
+    char first = str[0];
+    string sub = str.Substring(1);
+    HashSet<string> words = GetPermutations(sub);
+    foreach (string word in words)
+    {
+        for (int i = 0; i <= word.Length; i++)
+            permutations.Add(word.Substring(0, i) + first + word.Substring(i));
+    }
+    return permutations;
 }
