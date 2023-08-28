a70fa67a66433449f9289f731996df619304970b
karthickraj <karthickraj.s@metamation.com>
2023-08-28 11:04:38 +0530

    Implemented reduce string

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿// Reduce string
+Console.Write("Enter the string: ");
+string s = Console.ReadLine();
+bool reduced;
+do
 {
-    private static void Main(string[] args)
+    reduced = false;
+    for (int i = 0; i < s.Length - 1; i++)
     {
-        Console.WriteLine("Hello, World!");
+        if (s[i] == s[i + 1])
+        {
+            s = s.Substring(0, i) + s.Substring(i + 2);
+            reduced = true;
+        }
     }
-}
+} while (reduced);
+if (s.Length == 0)
+    Console.WriteLine("Empty String");
+else
+    Console.WriteLine($"The reduced string is {s}");
