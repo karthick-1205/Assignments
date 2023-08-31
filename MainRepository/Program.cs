9c218f8e750c488ff381a2e1231ab7f893ce5842
karthickraj <karthickraj.s@metamation.com>
2023-08-31 16:18:11 +0530

    Implemented factorial program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿//Factorial program
+for (; ; )
 {
-    private static void Main(string[] args)
+    Console.Write("Enter the number or press [e] to exit: ");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "e") break;
+    int.TryParse(input, out int n);
+    if (n < 0) Console.WriteLine("Enter the valid input");
+    else
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("The factorial of {0} is {1}", n, factorial(n));
+        static int factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * factorial(n - 1);
+        }
     }
 }
