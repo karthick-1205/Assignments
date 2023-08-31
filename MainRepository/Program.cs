2d592b37791441ce3875142281d526c7096c63de
karthickraj <karthickraj.s@metamation.com>
2023-08-31 12:56:54 +0530

    Implemented longest abecedarian word

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿//LONGEST ABECEDARIAN WORD
+string[] arr = { "almost", "bee", "apple", "school", "in" };
+List<string> list = new();
+foreach (var res in arr)
 {
-    private static void Main(string[] args)
+    if (!isAbecedarian(res)) continue;
+    else list.Add(res);
+}
+if (list.Count == 0) Console.WriteLine("");
+string longWord = list.OrderByDescending(x => x.Length).First();
+Console.WriteLine($"The given {longWord} is Longest Abecedarian word");
+static bool isAbecedarian(string s)
+{
+    for (int i = 0; i < s.Length - 1; i++)
     {
-        Console.WriteLine("Hello, World!");
+        if (s[i] > s[i + 1]) return false;
     }
+    return true;
 }
