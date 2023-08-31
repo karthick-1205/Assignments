030dc6fbfce85bf061a18d46305aa164b96d8b83
karthickraj <karthickraj.s@metamation.com>
2023-08-31 15:35:41 +0530

    Implemented swap two values

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿Console.Write("Enter the value of a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the value of b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Before Swapping a= {a}, b = {b}");
+swapNumber(a, b);
+static void swapNumber(int a, int b)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    int temp = a;
+    a = b;
+    b = temp;
+    Console.WriteLine($"After Swapping a= {a}, b = {b}");
 }
