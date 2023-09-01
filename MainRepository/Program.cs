54cd0c69a581203ca8a424a0f18b6ee9a691f070
karthickraj <karthickraj.s@metamation.com>
2023-09-01 17:32:14 +0530

    Implemented Armstrong program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿//Armstrong number
+for (; ; )
 {
-    private static void Main(string[] args)
+    Console.Write("Enter the number or press [e] to exit: ");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "e") break;
+    if (int.TryParse(input, out int n) && n > 0)
     {
-        Console.WriteLine("Hello, World!");
+        bool result = ArmStrong(n);
+        if (!result) Console.WriteLine($"The given {n} number is not a armstrong number");
+        else Console.WriteLine($"The given {n} number is armstrong number");
+    }
+    else Console.WriteLine("Enter the valid input");
+    static bool ArmStrong(int n)
+    {
+        int sum = 0, temp = n, digits = 0, temp1 = n;
+        while (temp > 0)
+        {
+            temp /= 10;
+            digits++;
+        }
+        while (temp1 > 0)
+        {
+            int rem = temp1 % 10;
+            sum += (int)Math.Pow(rem, digits);
+            temp1 /= 10;
+        }
+        if (n != sum) return false;
+        return true;
     }
 }
