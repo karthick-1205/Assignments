2f47d86ed15c30a9698e2094c87283ace786a82e
karthickraj <karthickraj.s@metamation.com>
2023-09-06 14:34:00 +0530

    Implemented nth armstrong number

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -2,6 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (int.TryParse(args[0], out int n) && n > 0 && n <= 25) Console.WriteLine($"The {n}th armstrong number is {Armstrong(n)}");
+        else Console.WriteLine("Enter the valid input");
+    }
+
+    static int Armstrong(int n)
+    {
+        int count = 0;
+        for (int l = 1; l <= int.MaxValue; l++)
+        {
+            int digits, sum = 0, temp = l, rem;
+            digits = l.ToString().Length;
+            while (temp > 0)
+            {
+                rem = temp % 10;
+                sum += (int)Math.Pow(rem, digits);
+                temp /= 10;
+            }
+            if (l == sum) count++;
+            if (count == n) return l;
+        }
+        return n;
     }
 }
