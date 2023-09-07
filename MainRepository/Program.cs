36d5b5d20e0eb739233237639bd351c014b89de2
karthickraj <karthickraj.s@metamation.com>
2023-09-07 11:33:36 +0530

    Review fixed1

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -6,20 +6,19 @@ for (; ; )
     if (input.ToLower() == "e") break;
     if (int.TryParse(input, out int n) && n > 0)
     {
-        bool result = Armstrong(n);
+        bool result = Armstrong(n, input.Length);
         if (result) Console.WriteLine($"The given {n} number is armstrong number");
         else Console.WriteLine($"The given {n} number is not a armstrong number");
     }
     else Console.WriteLine("Enter the valid input");
 
-    static bool Armstrong(int n)
+    static bool Armstrong(int n, int len)
     {
         int sum = 0, temp = n;
-        int digits = n.ToString().Length;
         while (temp > 0)
         {
             int rem = temp % 10;
-            sum += (int)Math.Pow(rem, digits);
+            sum += (int)Math.Pow(rem, len);
             temp /= 10;
         }
         if (n != sum) return false;
