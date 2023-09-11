71cd85c7abeb704d7635d9e5cb23cad70557d5a1
karthickraj <karthickraj.s@metamation.com>
2023-09-11 15:48:00 +0530

    Implemented chess program

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿// Print chessboard
+int n = 8;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+string whiteCoins = " ♖ │ ♘ │ ♗ │ ♕ │ ♔ │ ♗ │ ♘ │ ♖ │";
+string blackPawns = " ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │";
+string whitePawns = " ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │";
+string blackCoins = " ♜ │ ♞ │ ♝ │ ♛ │ ♚ │ ♝ │ ♞ │ ♜ │";
+Console.WriteLine("┌───┬───┬───┬───┬───┬───┬───┬───┐");
+for (int i = 0; i < n; i++)
 {
-    private static void Main(string[] args)
+    Console.Write("│");
+    if (i == 0) Console.Write(whiteCoins);
+    else if (i == 1) Console.Write(blackPawns);
+    else if (i == n - 2) Console.Write(whitePawns);
+    else if (i == n - 1) Console.Write(blackCoins);
+    else
     {
-        Console.WriteLine("Hello, World!");
+        for (int j = 0; j < n; j++) Console.Write("   │");
     }
+    if (i == n - 1) break;
+    Console.WriteLine("\n├───┼───┼───┼───┼───┼───┼───┼───┤");
 }
+Console.WriteLine("\n└───┴───┴───┴───┴───┴───┴───┴───┘");
