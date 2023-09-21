8aad6a5cc521d3b0219c0f49fa6ee85ce24d97f7
karthickraj <karthickraj.s@metamation.com>
2023-09-21 15:16:53 +0530

    Implemented Votingcontest

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+﻿while (true) {
+   Console.Write ("Enter the total number of votes by the member ");
+   string input = Console.ReadLine ().ToLower ().Replace (" ", "");
+   if (input.ToLower () == "e") break;
+   if (input == "") Console.WriteLine ("Enter the valid input");
+   else {
+      VotingContest (input, out int totalVotes);
+      Console.WriteLine ($"The total votes is {totalVotes}");
+   }
+}
+
+static void VotingContest (string s, out int votes) {
+   Dictionary<char, int> voteCount = new ();
+   foreach (char c in s) {
+      var result = voteCount.ContainsKey (c) ? voteCount[c]++ : voteCount[c] = 1;
+   }
+   var winner = voteCount.OrderByDescending (x => x.Value).First ();
+   Console.WriteLine ($"The winner is {winner.Key}");
+   votes = winner.Value;
 }
