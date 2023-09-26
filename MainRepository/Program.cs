3388f1a95f47f8cfa22695cd93cf552599010201
karthickraj <karthickraj.s@metamation.com>
2023-09-26 17:58:36 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -8,14 +8,13 @@ string blackCoins = " ♜ │ ♞ │ ♝ │ ♛ │ ♚ │ ♝ │ ♞ │ �
 Console.WriteLine ("┌───┬───┬───┬───┬───┬───┬───┬───┐");
 for (int i = 0; i < n; i++) {
    Console.Write ("│");
+   bool end = i == n - 1;
    if (i == 0) Console.Write (whiteCoins);
    else if (i == 1) Console.Write (blackPawns);
    else if (i == n - 2) Console.Write (whitePawns);
-   else if (i == n - 1) Console.Write (blackCoins);
-   else {
-      for (int j = 0; j < n; j++) Console.Write ("   │");
-   }
-   if (i == n - 1) break;
+   else if (end) Console.Write (blackCoins);
+   else for (int j = 0; j < n; j++) Console.Write ("   │");
+   if (end) break;
    Console.WriteLine ("\n├───┼───┼───┼───┼───┼───┼───┼───┤");
 }
 Console.WriteLine ("\n└───┴───┴───┴───┴───┴───┴───┴───┘");
