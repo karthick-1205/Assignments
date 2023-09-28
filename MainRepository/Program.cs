a5a6847ba147ca6d5fc0ed9e6c3082db707825a5
karthickraj <karthickraj.s@metamation.com>
2023-09-28 10:55:14 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,18 +1,17 @@
 ﻿// Program to find the gcd and lcm of two numbers.
-Console.Write("Enter the first number: ");
+Console.Write ("Enter the first number: ");
 int n1, n2, x, y, temp, gcd, lcm;
-n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second number: ");
-n2 = Convert.ToInt32(Console.ReadLine());
+n1 = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Enter the second number: ");
+n2 = Convert.ToInt32 (Console.ReadLine ());
 x = n1;
 y = n2;
-while (y > 0)
-{
-    temp = y;
-    y = x % y;
-    x = temp;
+while (y > 0) {
+   temp = y;
+   y = x % y;
+   x = temp;
 }
 gcd = x;
 lcm = (n1 * n2) / gcd;
-Console.WriteLine("The GCD of two number is " + gcd);
-Console.WriteLine("The LCM of two number is " + lcm);
+Console.WriteLine ("The GCD of two number is " + gcd);
+Console.WriteLine ("The LCM of two number is " + lcm);
