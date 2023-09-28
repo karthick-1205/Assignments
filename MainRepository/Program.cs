27a35f0c9f710a86ce59063ca9e9f0ff40ddc7b9
karthickraj <karthickraj.s@metamation.com>
2023-09-28 11:26:34 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,19 +1,16 @@
 ﻿//LONGEST ABECEDARIAN WORD
 string[] arr = { "almost", "bee", "apple", "school", "in" };
-List<string> list = new();
-foreach (var res in arr)
-{
-    if (!isAbecedarian(res)) continue;
-    else list.Add(res);
+List<string> list = new ();
+foreach (var res in arr) {
+   if (!isAbecedarian (res)) continue;
+   else list.Add (res);
 }
-if (list.Count == 0) Console.WriteLine("");
-string longWord = list.OrderByDescending(x => x.Length).First();
-Console.WriteLine($"The given {longWord} is Longest Abecedarian word");
-static bool isAbecedarian(string s)
-{
-    for (int i = 0; i < s.Length - 1; i++)
-    {
-        if (s[i] > s[i + 1]) return false;
-    }
-    return true;
+if (list.Count == 0) Console.WriteLine ("");
+string longWord = list.OrderByDescending (x => x.Length).First ();
+Console.WriteLine ($"The given {longWord} is Longest Abecedarian word");
+static bool isAbecedarian (string s) {
+   for (int i = 0; i < s.Length - 1; i++) {
+      if (s[i] > s[i + 1]) return false;
+   }
+   return true;
 }
