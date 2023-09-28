18a6fe1034d6512961a187a80330f16e1abc4f2e
karthickraj <karthickraj.s@metamation.com>
2023-09-28 11:21:37 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,23 +1,20 @@
 ﻿//String permutation
-Console.Write("Enter the string: ");
-string str = Console.ReadLine();
-HashSet<string> result = GetPermutations(str);
-Console.WriteLine($"Permutation of {str} are " + string.Join(" ", result));
-static HashSet<string> GetPermutations(string str)
-{
-    HashSet<string> permutations = new ();
-    if (str.Length == 0)
-    {
-        permutations.Add("");
-        return permutations;
-    }
-    char first = str[0];
-    string sub = str.Substring(1);
-    HashSet<string> words = GetPermutations(sub);
-    foreach (string word in words)
-    {
-        for (int i = 0; i <= word.Length; i++)
-            permutations.Add(word.Substring(0, i) + first + word.Substring(i));
-    }
-    return permutations;
+Console.Write ("Enter the string: ");
+string str = Console.ReadLine ();
+HashSet<string> result = GetPermutations (str);
+Console.WriteLine ($"Permutation of {str} are " + string.Join (" ", result));
+static HashSet<string> GetPermutations (string str) {
+   HashSet<string> permutations = new ();
+   if (str.Length == 0) {
+      permutations.Add ("");
+      return permutations;
+   }
+   char first = str[0];
+   string sub = str.Substring (1);
+   HashSet<string> words = GetPermutations (sub);
+   foreach (string word in words) {
+      for (int i = 0; i <= word.Length; i++)
+         permutations.Add (word.Substring (0, i) + first + word.Substring (i));
+   }
+   return permutations;
 }
