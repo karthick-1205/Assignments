c4fd034d14b8511f69ee7f1d1957b1d7ffc49165
karthickraj <karthickraj.s@metamation.com>
2023-09-28 11:28:09 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,11 +1,10 @@
-﻿Console.Write("Enter the value of a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the value of b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Before Swapping a= {a}, b = {b}");
-swapNumber(a, b);
-static void swapNumber(int a, int b)
-{
-    (a, b) = (b, a);
-    Console.WriteLine($"After Swapping a= {a}, b = {b}");
+﻿Console.Write ("Enter the value of a: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Enter the value of b: ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ($"Before Swapping a= {a}, b = {b}");
+swapNumber (a, b);
+static void swapNumber (int a, int b) {
+   (a, b) = (b, a);
+   Console.WriteLine ($"After Swapping a= {a}, b = {b}");
 }
