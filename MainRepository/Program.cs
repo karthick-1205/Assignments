ef011b9bc735f8ccba355c366067bc5e28e6f5f0
karthickraj <karthickraj.s@metamation.com>
2023-09-28 10:45:41 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,36 +1,31 @@
 ﻿// Program to convert the decimal number to binary and hexadecimal number.
-Console.Write("Enter the number: ");
+Console.Write ("Enter the number: ");
 int number, num, num1;
-number = Convert.ToInt32(Console.ReadLine());
+number = Convert.ToInt32 (Console.ReadLine ());
 num = num1 = number;
-List<int> binary = new();
-List<char> hexadecimal = new();
-while (number > 0)
-{
-    int temp = number % 2;
-    binary.Add(temp);
-    number /= 2;
+List<int> binary = new ();
+List<char> hexadecimal = new ();
+while (number > 0) {
+   int temp = number % 2;
+   binary.Add (temp);
+   number /= 2;
 }
-while (num1 > 0)
-{
-    int temp = num1 % 16;
-    if (temp < 10)
-    {
-        temp = temp + 48;
-        hexadecimal.Add((char)temp);
-    }
-    else
-    {
-        temp = temp + 55;
-        hexadecimal.Add((char)temp);
-    }
-    num1 /= 16;
+while (num1 > 0) {
+   int temp = num1 % 16;
+   if (temp < 10) {
+      temp += 48;
+      hexadecimal.Add ((char)temp);
+   } else {
+      temp += 55;
+      hexadecimal.Add ((char)temp);
+   }
+   num1 /= 16;
 }
-binary.Reverse();
-hexadecimal.Reverse();
-Console.Write($"The binary number of {num} is ");
+binary.Reverse ();
+hexadecimal.Reverse ();
+Console.Write ($"The binary number of {num} is ");
 foreach (var result in binary)
-    Console.Write(result);
-Console.Write($"\nThe hexadecimal number of {num} is ");
+   Console.Write (result);
+Console.Write ($"\nThe hexadecimal number of {num} is ");
 foreach (var result in hexadecimal)
-    Console.Write(result);
+   Console.Write (result);
