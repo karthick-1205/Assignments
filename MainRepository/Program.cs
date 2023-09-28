d7e12b01cd0668edf44ac6cceb897c83691b1b25
karthickraj <karthickraj.s@metamation.com>
2023-09-28 11:18:19 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,20 +1,17 @@
 ﻿// Reduce string
-Console.Write("Enter the string: ");
-string s = Console.ReadLine();
+Console.Write ("Enter the string: ");
+string s = Console.ReadLine ();
 bool reduced;
-do
-{
-    reduced = false;
-    for (int i = 0; i < s.Length - 1; i++)
-    {
-        if (s[i] == s[i + 1])
-        {
-            s = s.Substring(0, i) + s.Substring(i + 2);
-            reduced = true;
-        }
-    }
+do {
+   reduced = false;
+   for (int i = 0; i < s.Length - 1; i++) {
+      if (s[i] == s[i + 1]) {
+         s = s.Substring (0, i) + s.Substring (i + 2);
+         reduced = true;
+      }
+   }
 } while (reduced);
 if (s.Length == 0)
-    Console.WriteLine("Empty String");
+   Console.WriteLine ("Empty String");
 else
-    Console.WriteLine($"The reduced string is {s}");
+   Console.WriteLine ($"The reduced string is {s}");
