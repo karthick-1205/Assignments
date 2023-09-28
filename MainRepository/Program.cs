4f854a06c3a97ccc08f176e533fe02beb024f98c
karthickraj <karthickraj.s@metamation.com>
2023-09-28 11:15:46 +0530

    Fixed code format

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,17 +1,15 @@
 ﻿// Print Pascal's triangle
-Console.Write("Enter no of Lines: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Enter no of Lines: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
 int spaces = n;
-for (int i = 0; i < n; i++)
-{
-    for (int s = 1; s <= spaces; s++)
-        Console.Write(" ");
-    int num = 1;
-    for (int j = 0; j <= i; j++)
-    {
-        Console.Write(num + " ");
-        num = num * (i - j) / (j + 1);
-    }
-    spaces--;
-    Console.WriteLine();
+for (int i = 0; i < n; i++) {
+   for (int s = 1; s <= spaces; s++)
+      Console.Write (" ");
+   int num = 1;
+   for (int j = 0; j <= i; j++) {
+      Console.Write (num + " ");
+      num = num * (i - j) / (j + 1);
+   }
+   spaces--;
+   Console.WriteLine ();
 }
