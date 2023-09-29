65efbecb7101357f09c718d9e48b3c793deedc6c
karthickraj <karthickraj.s@metamation.com>
2023-09-29 18:57:25 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,31 +1,35 @@
 ﻿// Program to convert the decimal number to binary and hexadecimal number.
 Console.Write ("Enter the number: ");
-int number, num, num1;
-number = Convert.ToInt32 (Console.ReadLine ());
-num = num1 = number;
+string input = Console.ReadLine ();
 List<int> binary = new ();
 List<char> hexadecimal = new ();
-while (number > 0) {
-   int temp = number % 2;
-   binary.Add (temp);
-   number /= 2;
-}
-while (num1 > 0) {
-   int temp = num1 % 16;
-   if (temp < 10) {
-      temp += 48;
-      hexadecimal.Add ((char)temp);
-   } else {
-      temp += 55;
-      hexadecimal.Add ((char)temp);
+if (int.TryParse (input, out int number) && number > 0) {
+   while (number > 0) {
+      binary.Add (number % 2);
+      number /= 2;
    }
-   num1 /= 16;
-}
-binary.Reverse ();
-hexadecimal.Reverse ();
-Console.Write ($"The binary number of {num} is ");
-foreach (var result in binary)
-   Console.Write (result);
-Console.Write ($"\nThe hexadecimal number of {num} is ");
-foreach (var result in hexadecimal)
-   Console.Write (result);
+   number = int.Parse (input);
+   while (number > 0) {
+      int temp = number % 16;
+      if (temp < 10) {
+         // if temp value range from 0 to 9, then we have to add 48 to convert integer value to ascii character.  
+         temp += 48;
+         hexadecimal.Add ((char)temp);
+      } else {
+         // if temp value is above 9, then we have to add 55 to convert integer value to ascii character.  
+         temp += 55;
+         hexadecimal.Add ((char)temp);
+      }
+      number /= 16;
+   }
+   binary.Reverse ();
+   hexadecimal.Reverse ();
+   Console.Write ($"The binary number of {input} is ");
+   foreach (var result in binary)
+      Console.Write (result);
+   Console.WriteLine ();
+   Console.Write ($"The hexadecimal number of {input} is ");
+   foreach (var result in hexadecimal)
+      Console.Write (result);
+} else Console.WriteLine ("Enter the valid input");
+
