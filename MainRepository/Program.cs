cb49d20e80c2d9049a2059440c48dcf84254eabf
karthickraj <karthickraj.s@metamation.com>
2023-09-29 12:16:57 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,7 @@
 ﻿while (true) {
    Console.Write ("Enter the total number of votes by the member ");
    string input = Console.ReadLine ().ToLower ().Replace (" ", "");
-   if (input.ToLower () == "e") break;
+   if (input.ToLower () == "q") break;
    if (input == "") Console.WriteLine ("Enter the valid input");
    else {
       VotingContest (input, out int totalVotes);
@@ -12,7 +12,8 @@
 static void VotingContest (string s, out int votes) {
    Dictionary<char, int> voteCount = new ();
    foreach (char c in s) {
-      var result = voteCount.ContainsKey (c) ? voteCount[c]++ : voteCount[c] = 1;
+      if (voteCount.ContainsKey (c)) voteCount[c]++;
+      else voteCount[c] = 1;
    }
    var winner = voteCount.OrderByDescending (x => x.Value).First ();
    Console.WriteLine ($"The winner is {winner.Key}");
