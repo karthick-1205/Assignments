659771b933b8b58c3cf4db866958e6ba8f29c6a0
karthickraj <karthickraj.s@metamation.com>
2023-09-29 13:19:08 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Program to find the fibonacci series of the number
 Console.Write ("Enter the Number: ");
+string input = Console.ReadLine ();
 int number, t1 = 0, t2 = 1, nextterm;
-number = Convert.ToInt32 (Console.ReadLine ());
-Console.Write ($"Fibonacci series of number f({number}) is ");
-for (int i = 0; i < number; i++) {
-   Console.Write ("{0} ", t1);
-   nextterm = t1 + t2;
-   (t1, t2) = (t2, nextterm);
-}
+if (int.TryParse (input, out number) && number > 0) {
+   Console.Write ($"Fibonacci series of number f({number}) is ");
+   for (int i = 0; i < number; i++) {
+      Console.Write ("{0} ", t1);
+      nextterm = t1 + t2;
+      (t1, t2) = (t2, nextterm);
+   }
+} else Console.WriteLine ("Enter the valid input");
