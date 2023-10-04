9f440607925551e92adf2f011c9bf0b274588263
karthickraj <karthickraj.s@metamation.com>
2023-10-04 17:53:45 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,24 +1,18 @@
-﻿//Program to find the prime number or not.
-Console.Write ("Enter the Number: ");
-string number;
-number = Console.ReadLine ();
-Prime (number);
-static void Prime (string number) {
-   int result;
-   if (int.TryParse (number, out result)) {
-      int divisors = 0;
-      for (int i = 1; i <= result; i++) {
-         if (result % i == 0)
-            divisors++;
+﻿// Program to find the prime number or not.
+while (true) {
+   Console.Write ("Enter the Number: ");
+   string number = Console.ReadLine ();
+   if (int.TryParse (number, out int num) && num > 0) Prime (num);
+   else Console.WriteLine ("Enter the valid input");
+}
+static void Prime (int result) {
+   bool isPrime = true;
+   for (int i = 2; i < result; i++) {
+      if (result % i == 0) {
+         isPrime = false;
+         Console.WriteLine ($"{result} is the not a prime number");
+         break;
       }
-      if (divisors == 2)
-         Console.Write ($"{result} is the prime number");
-      else
-         Console.Write ($"{result} is the not a prime number");
-   } else {
-      Console.WriteLine ("Enter the valid input");
-      Console.Write ("Enter the Number: ");
-      number = Console.ReadLine ();
-      Prime (number);
    }
+   if (isPrime) Console.WriteLine ($"{result} is the prime number");
 }
