34b7aea2f0819689dc36fcf04e2eaccc68d109f1
karthickraj <karthickraj.s@metamation.com>
2023-10-04 13:06:25 +0530

    Review fixed2

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,8 +1,12 @@
 ﻿// Program to find the given word is palindrome or not.
 Console.Write ("Enter the word or sentence: ");
-string str = Console.ReadLine ().ToLower ().Replace (" ", "");
-string rev = new (str.Reverse ().ToArray ());
-if (rev == str)
-   Console.Write ("Palindrome");
-else
-   Console.Write ("Not a palindrome");
+string str = Console.ReadLine ()!.ToLower ().Replace (" ", "");
+bool isPalindrome = true;
+for (int i = 0; i < str.Length; i++) {
+   if (str[i] != str[str.Length - 1 - i]) {
+      isPalindrome = false;
+      break;
+   }
+}
+if (isPalindrome) Console.Write ("Palindrome");
+else Console.Write ("Not a palindrome");
