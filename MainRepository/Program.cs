3bdc834c4012023050014fdf410c67901922340d
karthickraj <karthickraj.s@metamation.com>
2023-10-04 11:07:48 +0530

    Review fixed2

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -2,7 +2,7 @@
 Console.Write ("Enter the number: ");
 List<int> binary = new ();
 List<char> hexadecimal = new ();
-List<char> hexValues = new () { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+string hexValues = "0123456789ABCDEF";
 if (!int.TryParse (Console.ReadLine (), out int input)) { Console.WriteLine ("Enter the valid input"); return; }
 int number = input;
 while (number > 0) {
