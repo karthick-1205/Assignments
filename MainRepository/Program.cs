e90c5f06344c2b937bbaa0e1ab8fcf1d9b74c969
karthickraj <karthickraj.s@metamation.com>
2023-10-10 15:18:22 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Program to print the multiplication table of the given number
 Console.Write ("Enter the Number: ");
-int n = Convert.ToInt32 (Console.ReadLine ());
-for (int i = 1; i <= 10; i++)
-   Console.WriteLine ($"{n} * {i,3} = {n * i}");
+if (int.TryParse (Console.ReadLine (), out int n) && n > 0) {
+   for (int i = 1; i <= 10; i++)
+      Console.WriteLine ($"{n} * {i,2} = {n * i}");
+} else Console.WriteLine ("Enter the valid input");
