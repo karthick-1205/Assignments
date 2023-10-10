1ec802811cc0dedbfe9809072d114bdc87a1f260
karthickraj <karthickraj.s@metamation.com>
2023-10-10 15:25:19 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Print Pascal's triangle
 Console.Write ("Enter no of Lines: ");
-int n = Convert.ToInt32 (Console.ReadLine ());
-int spaces = n;
-for (int i = 0; i < n; i++) {
-   for (int s = 1; s <= spaces; s++)
-      Console.Write (" ");
-   int num = 1;
-   for (int j = 0; j <= i; j++) {
-      Console.Write (num + " ");
-      num = num * (i - j) / (j + 1);
+if (int.TryParse (Console.ReadLine (), out int n) && n > 0) {
+   int spaces = n;
+   for (int i = 0; i < n; i++) {
+      for (int s = 0; s < spaces; s++)
+         Console.Write (" ");
+      int num = 1;
+      for (int j = 0; j <= i; j++) {
+         Console.Write (num + " ");
+         num = num * (i - j) / (j + 1);
+      }
+      spaces--;
+      Console.WriteLine ();
    }
-   spaces--;
-   Console.WriteLine ();
-}
+} else Console.WriteLine ("Enter the valid input");
