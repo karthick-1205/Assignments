b20ce8e1f691734e7c321151174bb97af2fff5ea
karthickraj <karthickraj.s@metamation.com>
2023-10-10 18:08:24 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,17 +1,13 @@
-﻿// Reduce string
+﻿using System.Text;
+// Reduce string
 Console.Write ("Enter the string: ");
 string s = Console.ReadLine ();
-bool reduced;
-do {
-   reduced = false;
-   for (int i = 0; i < s.Length - 1; i++) {
-      if (s[i] == s[i + 1]) {
-         s = s.Substring (0, i) + s.Substring (i + 2);
-         reduced = true;
-      }
-   }
-} while (reduced);
-if (s.Length == 0)
+StringBuilder reduceString = new StringBuilder ();
+for (int i = 0; i < s.Length; i++) {
+   if (i == s.Length - 1 || s[i] != s[i + 1]) reduceString.Append (s[i]);
+   else i++;
+}
+if (reduceString.Length == 0)
    Console.WriteLine ("Empty String");
 else
-   Console.WriteLine ($"The reduced string is {s}");
+   Console.WriteLine ($"The reduced string is {reduceString}");
