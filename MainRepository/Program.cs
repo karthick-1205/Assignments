3c6d15d846039ead681c9e0f2e766facc9cd9ade
karthickraj <karthickraj.s@metamation.com>
2023-10-12 16:08:32 +0530

    Reveiw fixed1

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,8 +1,7 @@
 ﻿// Program to find the gcd and lcm of two numbers.
 int n1 = GetNumber ("Enter the first number: ");
 int n2 = GetNumber ("Enter the second number: ");
-int x = n1;
-int y = n2;
+int x = n1, y = n2;
 while (y > 0) {
    int temp = y;
    y = x % y;
@@ -14,13 +13,13 @@ Console.WriteLine ($"The GCD of two number is {gcd}");
 Console.WriteLine ($"The LCM of two number is {lcm}");
 
 static int GetNumber (string message) {
-   string response;
-   int number = 0;
-   do {
+   Console.Write (message);
+   string response = Console.ReadLine ();
+   int number;
+   while (!int.TryParse (response, out number) || number <= 0) {
       if (number < 0) Console.WriteLine ("Enter the valid input");
       Console.Write (message);
       response = Console.ReadLine ();
    }
-   while (int.TryParse (response, out number) && number <= 0);
    return number;
 }
