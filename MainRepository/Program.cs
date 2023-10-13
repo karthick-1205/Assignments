e488ec61d21267bfc520563e387bf5f2dfd70779
karthickraj <karthickraj.s@metamation.com>
2023-10-13 19:05:07 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,10 +1,18 @@
-﻿Console.Write ("Enter the value of a: ");
-int a = Convert.ToInt32 (Console.ReadLine ());
-Console.Write ("Enter the value of b: ");
-int b = Convert.ToInt32 (Console.ReadLine ());
+﻿GetNumber ("Enter the value of a: ", out int a);
+GetNumber ("Enter the value of b: ", out int b);
 Console.WriteLine ($"Before Swapping a= {a}, b = {b}");
-swapNumber (a, b);
-static void swapNumber (int a, int b) {
+SwapNumber (a, b);
+
+static void SwapNumber (int a, int b) {
    (a, b) = (b, a);
    Console.WriteLine ($"After Swapping a= {a}, b = {b}");
+}
+
+static void GetNumber (string message, out int number) {
+   Console.Write (message);
+   while (!int.TryParse (Console.ReadLine (), out number) || number < 0) {
+      if (number > 0) break;
+      Console.WriteLine ("Enter the valid input");
+      Console.Write (message);
+   }
 }
