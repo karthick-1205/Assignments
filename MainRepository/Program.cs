4a46914280707f1d52ca18934068abde059f1b36
karthickraj <karthickraj.s@metamation.com>
2023-10-13 18:01:11 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -2,13 +2,14 @@
 string[] arr = { "almost", "bee", "apple", "school", "in" };
 List<string> list = new ();
 foreach (var res in arr) {
-   if (!isAbecedarian (res)) continue;
+   if (!IsAbecedarian (res)) continue;
    else list.Add (res);
 }
 if (list.Count == 0) Console.WriteLine ("");
 string longWord = list.OrderByDescending (x => x.Length).First ();
-Console.WriteLine ($"The given {longWord} is Longest Abecedarian word");
-static bool isAbecedarian (string s) {
+Console.WriteLine ($"The given '{longWord}' is Longest Abecedarian word");
+
+static bool IsAbecedarian (string s) {
    for (int i = 0; i < s.Length - 1; i++) {
       if (s[i] > s[i + 1]) return false;
    }
