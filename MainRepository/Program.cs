97dac7aded7a408db83f58f7f57104abd1555cb9
karthickraj <karthickraj.s@metamation.com>
2023-10-13 18:46:17 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Program to find the given number is palindrome number or not
 Console.Write ("Enter the number: ");
-int n = Convert.ToInt32 (Console.ReadLine ());
-int reverse = 0, temp;
-temp = n;
-while (temp > 0) {
-   int rem = temp % 10;
-   reverse = reverse * 10 + rem;
-   temp /= 10;
-}
-if (n == reverse)
-   Console.WriteLine ($"{reverse} is Palindrome number");
-else
-   Console.WriteLine ($"{reverse} is not a Palindrome number");
+if (int.TryParse (Console.ReadLine (), out int n) && n >= 0) {
+   int reverse = 0, temp;
+   temp = n;
+   while (temp > 0) {
+      int rem = temp % 10;
+      reverse = reverse * 10 + rem;
+      temp /= 10;
+   }
+   if (n == reverse) Console.WriteLine ($"{reverse} is Palindrome number");
+   else Console.WriteLine ($"{reverse} is not a Palindrome number");
+} else Console.WriteLine ("Enter the valid input");
