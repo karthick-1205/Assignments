ce48c0a980c054419ec452f8ab1f3a9bb69021d5
karthickraj <karthickraj.s@metamation.com>
2023-10-16 17:29:27 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,9 +1,13 @@
 ﻿//Isogram
 Console.Write ("Enter the string: ");
 string input = Console.ReadLine ();
+if (!input.Any (char.IsLetter)) {
+   Console.WriteLine ("Enter the valid input");
+   return;
+}
 Console.WriteLine ($"The given {input} string is " + (Isogram (input) ? "isogram" : "not a isogram"));
 static bool Isogram (string s) {
-   HashSet<char> Unique = new ();
-   foreach (char c in s) Unique.Add (c);
-   return s.Length == Unique.Count;
+   HashSet<char> unique = new ();
+   foreach (char c in s) unique.Add (c);
+   return s.Length == unique.Count;
 }
