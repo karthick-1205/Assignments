a9010ad9b67c45b42eb7680e01927a539ec02cc9
karthickraj <karthickraj.s@metamation.com>
2023-10-16 15:49:55 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,25 +1,20 @@
 ﻿// Program to find the digital root of given number.
-for (; ; )
-{
-   Console.Write ("Enter the Number or press [e] to exit: ");
-   string input = Console.ReadLine ();
-   if (input.ToLower () == "e") break;
-   int.TryParse (input, out int n);
-   int sum = 0;
-   if (n < 0) Console.WriteLine ("Enter the valid input");
-   else {
-      int temp = n;
-      while (temp > 0) {
-         int rem = temp % 10;
-         sum += rem;
-         temp /= 10;
-         if (temp == 0) {
-            if (sum > 9) {
-               temp = sum;
-               sum = 0;
-            }
-         }
-      }
-      Console.WriteLine ($"The digital root number of {n} is {sum}");
+int sum = 0, temp;
+for (; ; ) {
+   Console.Write ("Enter the Number: ");
+   if (!int.TryParse (Console.ReadLine (), out int n) || n < 0) {
+      Console.WriteLine ("Enter the valid input");
+      return;
    }
+   temp = n;
+   while (temp > 0) {
+      int rem = temp % 10;
+      sum += rem;
+      temp /= 10;
+      if (temp == 0 && sum > 9) {
+         temp = sum;
+         sum = 0;
+      }
+   }
+   Console.WriteLine ($"The digital root number of {n} is {sum}");
 }
