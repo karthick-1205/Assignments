7eea39936b17aecc2ad4c0c51f5066fc57ad66a2
karthickraj <karthickraj.s@metamation.com>
2023-10-16 15:08:46 +0530

    Review fixed2

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -3,27 +3,26 @@ Console.Write ("Enter the no of rows: ");
 if (!int.TryParse (Console.ReadLine (), out int n) || n < 0) {
    Console.WriteLine ("Enter the valid input");
    return;
-} else {
-   for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-         if (i + j >= n - 1) Console.Write ("* ");
-         else Console.Write ("  ");
-      }
-      for (int j = 0; j < n; j++) {
-         if (i > j) Console.Write ("* ");
-         else Console.Write ("  ");
-      }
-      Console.WriteLine ();
+}
+for (int i = 0; i < n; i++) {
+   for (int j = 0; j < n; j++) {
+      if (i + j >= n - 1) Console.Write ("* ");
+      else Console.Write ("  ");
    }
-   for (int i = 1; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-         if (i <= j) Console.Write ("* ");
-         else Console.Write ("  ");
-      }
-      for (int j = 0; j < n; j++) {
-         if (i + j < n - 1) Console.Write ("* ");
-         else Console.Write ("  ");
-      }
-      Console.WriteLine ();
+   for (int j = 0; j < n; j++) {
+      if (i > j) Console.Write ("* ");
+      else Console.Write ("  ");
    }
+   Console.WriteLine ();
+}
+for (int i = 1; i < n; i++) {
+   for (int j = 0; j < n; j++) {
+      if (i <= j) Console.Write ("* ");
+      else Console.Write ("  ");
+   }
+   for (int j = 0; j < n; j++) {
+      if (i + j < n - 1) Console.Write ("* ");
+      else Console.Write ("  ");
+   }
+   Console.WriteLine ();
 }
