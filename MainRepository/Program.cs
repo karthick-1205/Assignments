3aabcc6e481578bf59bc00c6670d8e0cf958e8b2
karthickraj <karthickraj.s@metamation.com>
2023-10-16 14:51:33 +0530

    Review fixed1

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Program to print a diamond using an asterisk
 Console.Write ("Enter the no of rows: ");
-if (int.TryParse (Console.ReadLine (), out int n) && n > 0) {
+if (!int.TryParse (Console.ReadLine (), out int n) || n < 0) {
+   Console.WriteLine ("Enter the valid input");
+   return;
+} else {
    for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
          if (i + j >= n - 1) Console.Write ("* ");
@@ -23,4 +26,4 @@ if (int.TryParse (Console.ReadLine (), out int n) && n > 0) {
       }
       Console.WriteLine ();
    }
-} else Console.WriteLine ("Enter the valid input");
+}
