f3736d2436ec76bc30e194aaceb2179935e3572f
karthickraj <karthickraj.s@metamation.com>
2023-10-16 15:23:34 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,16 +1,13 @@
 ﻿//Factorial program
-for (; ; )
-{
+for (; ; ) {
    Console.Write ("Enter the number or press [e] to exit: ");
-   string input = Console.ReadLine ();
-   if (input.ToLower () == "e") break;
-   int.TryParse (input, out int n);
-   if (n < 0) Console.WriteLine ("Enter the valid input");
-   else {
-      Console.WriteLine ("The factorial of {0} is {1}", n, factorial (n));
-      static int factorial (int n) {
-         if (n == 0) return 1;
-         return n * factorial (n - 1);
-      }
+   if (!int.TryParse (Console.ReadLine (), out int n) || n < 0) {
+      Console.WriteLine ("Enter the valid input");
+      return;
+   }
+   Console.WriteLine ("The factorial of {0} is {1}", n, factorial (n));
+   static int factorial (int n) {
+      if (n == 0) return 1;
+      return n * factorial (n - 1);
    }
 }
