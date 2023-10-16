cbb9dad03ff9567b8fec3ab20ef36b8b6102318e
karthickraj <karthickraj.s@metamation.com>
2023-10-16 13:02:55 +0530

    Review fixed1

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,19 +1,29 @@
 ﻿// Program to find user is supposed to guess a number between 0 and 100 in a maximum of 8 attempts.
+using System.Diagnostics.Metrics;
+
 namespace Test2 {
    class Program {
       static void Main (string[] args) {
          int rand = new Random ().Next (1, 101), tryCount = 0;
          while (tryCount < 8) {
-            Console.WriteLine ("Enter your guess(1-100): ");
-            if (int.TryParse (Console.ReadLine (), out int number) && number >= 0) {
+            Console.WriteLine ("Enter your guess (1-100): ");
+            if (!int.TryParse (Console.ReadLine (), out int number) || number < 0) {
+               Console.WriteLine ("Enter the valid input");
+            } else {
                tryCount++;
-               if (number == rand) {
-                  Console.WriteLine ("Correct you Win");
-                  Console.WriteLine ("No of tries:{0}", tryCount);
-                  break;
-               } else if (rand > number) Console.WriteLine ("The number is higher and guess again");
-               else Console.WriteLine ("The number is lesser and guess again");
-            } else Console.WriteLine ("Enter the valid input");
+               switch (rand.CompareTo (number)) {
+                  case 1:
+                     Console.WriteLine ("The number is higher, guess again.");
+                     break;
+                  case -1:
+                     Console.WriteLine ("The number is lesser, guess again.");
+                     break;
+                  default:
+                     Console.WriteLine ("Correct, you Win!");
+                     Console.WriteLine ("Number of tries: {0}", tryCount);
+                     break;
+               }
+            }
          }
          if (tryCount == 8) {
             Console.WriteLine ("you lose game");
