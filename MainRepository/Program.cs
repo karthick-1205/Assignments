5e59718c6f235dc3c5eaa2db38c80e7a97efcf75
karthickraj <karthickraj.s@metamation.com>
2023-10-20 15:55:52 +0530

    Review fixed1

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System.Text;
 // Reduce string
-Console.Write ("Enter the string: ");
-string s = Console.ReadLine ();
-StringBuilder reduceString = new StringBuilder ();
-for (int i = 0; i < s.Length; i++) {
-   if (i == s.Length - 1 || s[i] != s[i + 1]) reduceString.Append (s[i]);
-   else i++;
+var inputString = new List<string> { "a", "bb", "aabb", "aabcbbaa", "aabbcdddc", "aacbbcdbda" };
+foreach (string input in inputString) Reduce (input);
+
+static void Reduce (string s) {
+   StringBuilder reduceString = new ();
+   for (int i = 0; i < s.Length; i++) {
+      if (i == s.Length - 1 || s[i] != s[i + 1]) reduceString.Append (s[i]);
+      else i++;
+   }
+   if (reduceString.Length == 0)
+      Console.WriteLine ("Empty String");
+   else
+      Console.WriteLine ($"The reduced string is {reduceString}");
 }
-if (reduceString.Length == 0)
-   Console.WriteLine ("Empty String");
-else
-   Console.WriteLine ($"The reduced string is {reduceString}");
