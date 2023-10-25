90dc774b2d3a60e2c89272f4f65a14e2a189da27
karthickraj <karthickraj.s@metamation.com>
2023-10-25 12:28:24 +0530

    Review fixed2

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System.Text;
 // Reduce string
 var inputString = new List<string> { "a", "bb", "aabb", "aabcbbaa", "aabbcdddc", "aacbbcdbda" };
+Dictionary<string, string> dictString = new () { { "a", "a" }, { "bb", "" }, { "aabb", "" }, { "aabcbbaa", "bc" }, { "aabbcdddc", "cdc" }, { "aacbbcdbda", "ccdbda" } };
+var reduceStringList = new List<string> ();
 foreach (string input in inputString) Reduce (input);
+foreach (var value in dictString.Values) {
+   if (reduceStringList.Contains (value)) Console.WriteLine ("Both string values are equal");
+   else Console.WriteLine ("Both string values are not equal");
+}
 
-static void Reduce (string s) {
+void Reduce (string s) {
    StringBuilder reduceString = new ();
    for (int i = 0; i < s.Length; i++) {
       if (i == s.Length - 1 || s[i] != s[i + 1]) reduceString.Append (s[i]);
       else i++;
    }
-   if (reduceString.Length == 0)
-      Console.WriteLine ("Empty String");
-   else
-      Console.WriteLine ($"The reduced string is {reduceString}");
+   reduceStringList.Add (reduceString.ToString ());
 }
