81ccecfd7e289d6d0a3262ad834d0ff2ffdfbaef
karthickraj <karthickraj.s@metamation.com>
2023-10-26 14:39:11 +0530

    Review fixed3

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System.Text;
 // Reduce string
-var inputString = new List<string> { "a", "bb", "aabb", "aabcbbaa", "aabbcdddc", "aacbbcdbda" };
-Dictionary<string, string> dictString = new () { { "a", "a" }, { "bb", "" }, { "aabb", "" }, { "aabcbbaa", "bc" }, { "aabbcdddc", "cdc" }, { "aacbbcdbda", "ccdbda" } };
-var reduceStringList = new List<string> ();
-foreach (string input in inputString) Reduce (input);
-foreach (var value in dictString.Values) {
-   if (reduceStringList.Contains (value)) Console.WriteLine ("Both string values are equal");
-   else Console.WriteLine ("Both string values are not equal");
+Dictionary<string, string> dict = new () { { "a", "a" }, { "bb", "" }, { "aabb", "" }, { "aabcbbaa", "bc" }, { "aabbcdddc", "cdc" }, { "aacbbcdbda", "ccdbda" }, { "aabcb", "bc" } };
+foreach (string input in dict.Keys) {
+   var result = Reduce (input);
+   if (result.Equals (dict[input])) {
+      Console.WriteLine ($"The input string of '{result}'='{dict[input]}' dictionary values are equal");
+   } else Console.WriteLine ($"The input string of '{result}'='{dict[input]}' dictionary values are not equal");
 }
 
-void Reduce (string s) {
+static string Reduce (string s) {
    StringBuilder reduceString = new ();
    for (int i = 0; i < s.Length; i++) {
       if (i == s.Length - 1 || s[i] != s[i + 1]) reduceString.Append (s[i]);
       else i++;
    }
-   reduceStringList.Add (reduceString.ToString ());
+   return reduceString.ToString ();
 }
