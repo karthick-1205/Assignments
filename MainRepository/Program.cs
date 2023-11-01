cb7f7bef8eaa7e0cdc26d20e2b8823187d2d8c75
karthickraj <karthickraj.s@metamation.com>
2023-11-01 15:14:34 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -6,10 +6,7 @@ foreach (var word in words)
    if (IsValid (word)) valid.Add (word);
 for (int i = 0; i < valid.Count - 1; i++) {
    for (int j = i + 1; j < valid.Count; j++) {
-      if (InOrder (valid[i], valid[j])) {
-         (valid[j], valid[i]) = (valid[i], valid[j]);
-      }
-
+      if (!InOrder (valid[i], valid[j])) (valid[j], valid[i]) = (valid[i], valid[j]);
    }
 }
 int total = 0;
@@ -21,21 +18,25 @@ foreach (var w in valid) {
    total += score;
 }
 Console.WriteLine ($"---\n{total,3} total");
+
 // Checking the given string is valid or not
 bool IsValid (string word) {
    if ((word.Length >= 4) && (word.Contains (seed[0])) && (word.All (seed.Contains))) return true;
    return false;
 }
+
 // Checking the given string is pangram or not
 bool IsPangram (string word) {
    return seed.All (word.Contains);
 }
+
 // Compute the score of given string
 int GetScore (string word) {
    if (word.Length <= 4) return 1;
    else if (IsPangram (word)) return word.Length + 7;
-   else return word.Length;
+   return word.Length;
 }
+
 // Comparing the score of given string
 bool InOrder (string a, string b) {
    int sa = GetScore (a), sb = GetScore (b);
