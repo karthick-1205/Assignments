f733a938c7bcc7b63eac445eb4463e7721612781
karthickraj <karthickraj.s@metamation.com>
2023-11-01 13:11:17 +0530

    Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -2,24 +2,28 @@
 namespace Test2 {
    class GuessNumber {
       public static void GuessGame () {
-         int n = 2;
-         List<int> BitList = new List<int> () { 0, 0, 0, 0, 0, 0, 0 };
-         List<int> Powers = new List<int> () { 64, 32, 16, 8, 4, 2, 1 };
+         int[] bits = { 0, 0, 0, 0, 0, 0, 0 };
+         int[] powers = { 64, 32, 16, 8, 4, 2, 1 };
          Console.Write ("Is your number is odd or even? (o/e): ");
-         string firstDigit = Console.ReadLine ();
-         if (firstDigit == "o" || firstDigit == "O") BitList[6] = 1;
+         var firstDigit = Console.ReadKey ().Key;
+         if (!(firstDigit is ConsoleKey.O or ConsoleKey.E)) {
+            Console.WriteLine ("Enter valid input");
+            return;
+         }
+         if (firstDigit is ConsoleKey.O) bits[6] = 1;
+         Console.WriteLine ();
+         int n = 2;
          for (int i = 5; i >= 0; i--) {
             Console.Write ($"If the number gets divided by {(Math.Pow (2, n))}, will it give a remainder of any number in the range [{(Math.Pow (2, n - 1))}, {(Math.Pow (2, n) - 1)}]? (y/n): ");
-            string nthDigit = Console.ReadLine ();
-            if (nthDigit == "y" || nthDigit == "Y") BitList[i] = 1;
+            string nthDigit = Console.ReadLine ()!;
+            if (nthDigit == "y" || nthDigit == "Y") bits[i] = 1;
             n++;
          }
-         for (int i = 0; i <= 6; i++) {
-            Powers[i] = Powers[i] * BitList[i];
-         }
-         Console.Write ($"I got the answer is 0b{String.Join ("", BitList)} = {Powers.Sum ()}.");
+         for (int i = 0; i <= 6; i++) powers[i] = powers[i] * bits[i];
+         Console.Write ($"I got the answer is 0b{string.Join ("", bits)} = {powers.Sum ()}.");
          Console.WriteLine ();
       }
+
       public static void Main (string[] args) {
          Console.WriteLine ("Choose number between 1 and 127");
          Console.WriteLine ("Decide number");
