1351114e4d5a14564d23c33558c8100af4f52f80
karthickraj <karthickraj.s@metamation.com>
2023-11-03 15:01:36 +0530

    A02: Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -4,19 +4,22 @@ namespace Test2 {
       public static void GuessGame () {
          int[] bits = { 0, 0, 0, 0, 0, 0, 0 };
          int[] powers = { 64, 32, 16, 8, 4, 2, 1 };
-         Console.Write ("Is your number is odd or even? (o/e): ");
-         var firstDigit = Console.ReadKey ().Key;
-         if (!(firstDigit is ConsoleKey.O or ConsoleKey.E)) {
-            Console.WriteLine ("Enter valid input");
-            return;
-         }
+         ConsoleKey firstDigit;
+         do {
+            Console.Write ("Is your number is odd or even? (o/e): ");
+            firstDigit = Console.ReadKey ().Key;
+            if (firstDigit is not ConsoleKey.O and not ConsoleKey.E)
+               Console.WriteLine ("\nEnter valid input");
+            else break;
+         } while (true);
          if (firstDigit is ConsoleKey.O) bits[6] = 1;
          Console.WriteLine ();
          int n = 2;
          for (int i = 5; i >= 0; i--) {
             Console.Write ($"If the number gets divided by {(Math.Pow (2, n))}, will it give a remainder of any number in the range [{(Math.Pow (2, n - 1))}, {(Math.Pow (2, n) - 1)}]? (y/n): ");
-            string nthDigit = Console.ReadLine ()!;
-            if (nthDigit == "y" || nthDigit == "Y") bits[i] = 1;
+            var nthDigit = Console.ReadKey ().Key;
+            if (nthDigit is ConsoleKey.Y) bits[i] = 1;
+            Console.WriteLine ();
             n++;
          }
          for (int i = 0; i <= 6; i++) powers[i] = powers[i] * bits[i];
