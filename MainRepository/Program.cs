4082d37f5468e536f882824a057e99f9546b08b7
karthickraj <karthickraj.s@metamation.com>
2023-11-08 15:11:09 +0530

    A15: Refactor changes

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,57 +1,56 @@
-﻿// Implementation of PriroityQueue using Binary Heap
-var pq = new PriorityQueue<int> ();
-pq.Enqueue (25);
-pq.Enqueue (8);
-Console.WriteLine (pq.Dequeue ());
-pq.Enqueue (27);
-pq.Enqueue (3);
-Console.WriteLine (pq.Dequeue ());
-pq.Enqueue (1);
-pq.Enqueue (2);
-pq.Enqueue (7);
-foreach (var item in pq.data)
+﻿var pq = new PriorityQueue<int> ();
+pq.EnQueue (25);
+pq.EnQueue (8);
+Console.WriteLine (pq.DeQueue ());
+pq.EnQueue (27);
+pq.EnQueue (3);
+Console.WriteLine (pq.DeQueue ());
+pq.EnQueue (1);
+pq.EnQueue (2);
+pq.EnQueue (7);
+foreach (var item in pq.mData)
    Console.Write (item + " ");
 Console.WriteLine ();
 
+/// <summary>Implementation of PriroityQueue using Binary Min Heap</summary>
 public class PriorityQueue<T> where T : IComparable<T> {
 
-   public List<T> data = new List<T> ();
+   public List<T> mData = new();
 
-   // Element inserting function maintain min heap
-   public void Enqueue (T item) {
-      data.Add (item);
-      int ci = data.Count - 1;
+   /// <summary>Adds element in the priority queue</summary>
+   public void EnQueue (T item) {
+      mData.Add (item);
+      int ci = mData.Count - 1;
       while (ci > 0) {
          int pi = (ci - 1) / 2;
-         if (data[ci].CompareTo (data[pi]) >= 0) break;
-         T tmp = data[ci];
-         data[ci] = data[pi];
-         data[pi] = tmp;
+         if (mData[ci].CompareTo (mData[pi]) >= 0) break;
+         (mData[pi], mData[ci]) = (mData[ci], mData[pi]);
          ci = pi;
       }
    }
 
-   // Element deleting function maintain min heap
-   public T Dequeue () {
-      int li = data.Count - 1;
-      T top = data[0];
-      data[0] = data[li];
-      data.RemoveAt (li);
+   /// <summary>Removes element in the priority queue</summary>
+   public T DeQueue () {
+      int li = mData.Count - 1;
+      T top = mData[0];
+      mData[0] = mData[li];
+      mData.RemoveAt (li);
       --li;
       int pi = 0;
       while (true) {
          int ci = pi * 2 + 1;
          if (ci > li) break;
          int rc = ci + 1;
-         if (rc <= li && data[rc].CompareTo (data[ci]) < 0) ci = rc;
-         if (data[pi].CompareTo (data[ci]) <= 0) break;
-         T tmp = data[pi];
-         data[pi] = data[ci];
-         data[ci] = tmp;
+         if (rc <= li && mData[rc].CompareTo (mData[ci]) < 0) ci = rc;
+         if (mData[pi].CompareTo (mData[ci]) <= 0) break;
+         (mData[ci], mData[pi]) = (mData[pi], mData[ci]);
          pi = ci;
       }
       return top;
    }
+
+   /// <summary>Checks element is present or not in the priority queue</summary>
    public bool IsEmpty => mCount == 0;
+
    int mCount;
 }
