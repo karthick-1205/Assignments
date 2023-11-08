71f8566c717a00fd20eeb61ebbbe9701c00646cc
karthickraj <karthickraj.s@metamation.com>
2023-11-08 16:45:56 +0530

    A14: Review fixed

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,21 +1,10 @@
-﻿/// <summary>Implementation of Anagram</summary>
-class Anagram {
-   public static void Main () {
-      Anagram a = new ();
-      a.FindAnagrams ();
-   }
-
-   /// <summary>This finds all anagrams and sorts them based on anagram count</summary>
-   public void FindAnagrams () {
-      foreach (string word in mWords) {
-         string sortedWord = new (word.OrderBy (c => c).ToArray ());
-         if (!mAnagrams.ContainsKey (sortedWord)) mAnagrams[sortedWord] = new List<string> ();
-         mAnagrams[sortedWord].Add (word);
-      }
-      foreach (KeyValuePair<string, List<string>> result in mAnagrams.Where (ab => ab.Value.Count > 1).OrderByDescending (a => a.Value.Count))
-         Console.WriteLine ($"{result.Value.Count,2} {string.Join (' ', result.Value)}");
-   }
-
-   string[] mWords = File.ReadAllLines ("c:/etc/words.txt");
-   Dictionary<string, List<string>> mAnagrams = new ();
+﻿/// <summary>This finds all anagrams and sorts them based on anagram count</summary>
+string[] words = File.ReadAllLines ("c:/etc/words.txt");
+Dictionary<string, List<string>> anagrams = new ();
+foreach (string word in words) {
+   string sortedWord = new (word.OrderBy (c => c).ToArray ());
+   if (!anagrams.TryGetValue (sortedWord, out var list)) anagrams.Add (sortedWord, list = new ());
+   list.Add (word);
 }
+foreach (KeyValuePair<string, List<string>> result in anagrams.Where (ab => ab.Value.Count > 1).OrderByDescending (a => a.Value.Count))
+   Console.WriteLine ($"{result.Value.Count,2} {string.Join (' ', result.Value)}");
