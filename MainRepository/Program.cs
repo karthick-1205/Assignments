952991d68aee56955df99ecf462c812085529b6b
karthickraj <karthickraj.s@metamation.com>
2023-11-10 13:32:02 +0530

    Implemented Queue

--- MainRepository/Program.cs
+++ MainRepository/Program.cs
@@ -1,7 +1,43 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+﻿internal class Program {
+   private static void Main () {
+   }
+}
+
+/// <summary>Implementation of circular queue class using array</summary>
+public class TQueue<T> {
+   /// <summary>Inserts elements into the queue</summary>
+   public void EnQueue (T a) {
+      if (mCount == mQueueArray.Length) {
+         var newArray = new T[mCount * 2];
+         for (int i = 0; i < mCount; i++)
+            newArray[i] = mQueueArray[(mFront + i) % mCount];
+         (mQueueArray, mFront, mRear) = (newArray, 0, mCount);
+      }
+      mQueueArray[mRear] = a;
+      mRear = (mRear + 1) % mQueueArray.Length;
+      mCount++;
+   }
+
+   /// <summary>Remove elements from the queue</summary>
+   public T DeQueue () {
+      if (IsEmpty) throw new InvalidOperationException ("Queue empty");
+      T a = mQueueArray[mFront];
+      mFront = (mFront + 1) % mQueueArray.Length;
+      mCount--;
+      return a;
+   }
+
+   /// <summary>Returns first element of the queue</summary>
+   public T Peek () {
+      if (IsEmpty) throw new InvalidOperationException ("Queue empty");
+      T a = mQueueArray[mFront];
+      return a;
+   }
+
+   /// <summary>Returns true if the queue is empty</summary>
+   public bool IsEmpty => mCount == 0;
+
+   T[] mQueueArray = new T[4];
+   int mCount, mFront, mRear;
+
 }
---
+++ QueueTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+
+namespace QueueTest {
+   [TestClass]
+   public class UnitTest1 {
+      [TestMethod]
+      public void TestEnqueueAndDequeue () {
+         TQueue<int> tQueue = new ();
+         Queue<int> queue = new ();
+         var listData = new List<int> { 4, 7, 1, 3, 8, 5, 9, 6 };
+         foreach (var v in listData) {
+            tQueue.EnQueue (v);
+            queue.Enqueue (v);
+         }
+         for (int i = 0; i < listData.Count; i++)
+            Assert.AreEqual (queue.Dequeue (), tQueue.DeQueue ());
+      }
+
+      [TestMethod]
+      public void TestPeek () {
+         TQueue<Double> tQueue = new ();
+         Queue<Double> queue = new ();
+         var listData = new List<Double> { 4.5, 7.2, 1.6, 3.1, 8.9, 5.3, 9.4, 6.8 };
+         foreach (var v in listData) {
+            tQueue.EnQueue (v);
+            queue.Enqueue (v);
+         }
+         Assert.AreEqual (queue.Peek (), tQueue.Peek ());
+      }
+
+      [TestMethod]
+      public void IsEmptyTest () {
+         TQueue<char> queue = new ();
+         Assert.IsTrue (queue.IsEmpty);
+         queue.EnQueue ('a');
+         Assert.IsFalse (queue.IsEmpty);
+         queue.DeQueue ();
+         Assert.IsTrue (queue.IsEmpty);
+      }
+
+      [TestMethod]
+      public void PopEmptyQueueTest () {
+         TQueue<string> queue = new ();
+         Assert.ThrowsException<InvalidOperationException> (() => queue.DeQueue ());
+      }
+
+      [TestMethod]
+      public void PeekEmptyQueueTest () {
+         TQueue<string> queue = new ();
+         Assert.ThrowsException<InvalidOperationException> (() => queue.Peek ());
+      }
+   }
+}
